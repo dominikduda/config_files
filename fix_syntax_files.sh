9f82ab4864eee92e572347ad9162e5bb1d0748ec
dominikduda <gospodorz@gmail.com>
2017-05-25 18:53:09 +0200

    Update fix_syntax_files.sh

--- fix_syntax_files.sh
+++ fix_syntax_files.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 function fix_file {
-  sed -i -e 's/contains=/contains=@Spell,/g' $1
-
+  sed -i -e '/exe/! {s/contains=/contains=@Spell,/g}' $1
   sed -i -e 's/contains=@Spell,ALL/contains=ALL/g' $1
   sed -i -e 's/contains=@Spell,ALLBUT/contains=ALLBUT/g' $1
   sed -i -e 's/contains=@Spell,TOP/contains=TOP/g' $1
   sed -i -e 's/contains=@Spell,CONTAINED/contains=CONTAINED/g' $1
   sed -i -e 's/contains=@Spell,NONE/contains=@Spell/g' $1
-
-  # Append 'contains=@Spell' to all lines containing 'syn match' and not containing 'contains='
-  sed -i -e '/syn match/ {/contains=/! s/$/ contains=@Spell/g}' $1
+  sed -i -e '/^ *syn match/ {/contains=/! s/$/ contains=@Spell/g}' $1
+  sed -i -e '/^ *syn region/ {/contains=/! s/$/ contains=@Spell/g}' $1
   return 0
 }
 
@@ -20,17 +18,17 @@ function revert_file {
 }
 
 function fix_recursively_in_catalog {
-  syntax_catalogs_paths="$(find $1 -type d -name 'syntax' -print)"
+  syntax_catalogs_paths="$(find $1 -type d ! -name '*.*' -not -path '*git*' -print)"
   syntax_catalogs_count="$(echo "${syntax_catalogs_paths}" | wc -l)"
 
   echo "${syntax_catalogs_count} syntax catalogs found and will be scanned for files"
 
   echo "${syntax_catalogs_paths}" | while read -r catalog_path ; do
-      echo "    $catalog_path"
-      ls "${catalog_path}" | grep -v .spellfix-backup | grep .vim | while read -r file_name ; do
+      echo "    Scanning $catalog_path"
+      ls -p "${catalog_path}" | grep -v / | grep -v .spellfix-backup | grep .vim | while read -r file_name ; do
           cp "${catalog_path}/${file_name}" "${catalog_path}/${file_name}.spellfix-backup"
           fix_file "${catalog_path}/${file_name}"
-          echo "        ${file_name} FIXED (backup created as ${file_name}.spellfix-backup)"
+          echo "        Fixing ${file_name} (backup created as ${file_name}.spellfix-backup)"
       done
   done
   echo 'Fix done.'
@@ -39,16 +37,16 @@ function fix_recursively_in_catalog {
 }
 
 function revert_recursively_in_catalog {
-  syntax_catalogs_paths="$(find $1 -type d -name 'syntax' -print)"
+  syntax_catalogs_paths="$(find $1 -type d ! -name '*.*' -not -path '*git*' -print)"
   syntax_catalogs_count="$(echo "${syntax_catalogs_paths}" | wc -l)"
 
   echo "${syntax_catalogs_count} syntax catalogs found and will be scanned for spellfix-backup files"
 
   echo "${syntax_catalogs_paths}" | while read -r catalog_path ; do
-      echo "    $catalog_path"
-      ls "${catalog_path}" | grep -v .spellfix-backup | grep .vim | while read -r file_name ; do
+      echo "    Scanning $catalog_path"
+      ls -p "${catalog_path}" | grep -v / | grep -v .spellfix-backup | grep .vim | while read -r file_name ; do
           revert_file "${catalog_path}" "${file_name}"
-          echo "        ${file_name} REVERTED (from file ${file_name}.spellfix-backup)"
+          echo "        Reverting ${file_name} (from file ${file_name}.spellfix-backup)"
       done
   done
   echo 'Revert done.'
@@ -57,9 +55,9 @@ function revert_recursively_in_catalog {
 }
 
 function main {
-  syntax_catalogs_paths="$(find $1 -type d -name 'syntax' -print)"
+  syntax_catalogs_paths="$(find $1 -type d ! -name '*.*' -not -path '*git*' -print)"
   while read -r catalog_path ; do
-      if ls "${catalog_path}" | grep .spellfix-backup; then
+      if ls -p "${catalog_path}" | grep -v / | grep .spellfix-backup; then
         echo ".spellfix-backup files found, reverting fix!"
         echo "--------------------------------------------"
         revert_recursively_in_catalog $1
@@ -71,4 +69,4 @@ function main {
   fix_recursively_in_catalog $1
 }
 
-main ~/.config/nvim/plug
+main ~/PATH/TO/VIM/PLUGINS/
