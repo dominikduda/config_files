9a22e36fffc87bfc092c6f779700b605031b424b
dominikduda <gospodorz@gmail.com>
2017-05-30 12:41:56 +0200

    Update bin/setup_md.sh - prevent script from applying changes twice

--- bin/setup_md.sh
+++ bin/setup_md.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-# 'a' stangs for after line, -i for editing file in place
-# /string_to_replace/<options> desired_multiline_string filename
-sed -i '/  class Application < Rails::Application/a \
-    config.middleware.insert_before 0, "Rack::Cors" do \
-      allow do \
-        origins "*" \
-        resource "*", headers: :any, methods: [:get, :post, :options] \
-      end \
-    end' config/application.rb
+function setup {
+  # 'a' stangs for after line, -i for editing file in place
+  # /string_to_replace/<options> desired_multiline_string filename
+  sed -i '/  class Application < Rails::Application/a \
+      config.middleware.insert_before 0, "Rack::Cors" do \
+        allow do \
+          origins "*" \
+          resource "*", headers: :any, methods: [:get, :post, :options] \
+        end \
+      end' config/application.rb
+  printf "gem 'rack-cors', require: 'rack/cors'" >> Gemfile
+  bundle
+  touch ran_scripts
+  printf "setup_md.sh -> true" >> ran_scripts
+}
 
-printf "gem 'rack-cors', require: 'rack/cors'" >> Gemfile
-
-bundle
+if grep "setup_md.sh -> true" ran_scripts; then
+  echo 'The script was already executed!'
+else
+  setup
+fi
