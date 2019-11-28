f0e40f39e83ebb9c30e98d1d9ccc40c200aa0503
dominikduda <gospodorz@gmail.com>
2019-11-28 18:34:12 +0100

    Add system_setup.sh

---
+++ system_setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Run me in following way: `sudo bash script.sh`
+
+echo "@>> You should only run this script on freshly installed system!"
+
+read -p "@>> Do you confirm that its the case? ('YES'): "
+if [ $REPLY = "YES" ]; then
+  echo "@>> Cool! Starting the script"
+else
+  echo "@>> Aborting the script"
+  exit
+fi
+
+# Check prerequisites
+
+wget --quiet --spider http://google.com
+if [ $? -eq 0 ]; then
+  echo '@>> Internet connection is working, cool!'
+else
+  echo  "@>> Internet connection is required for the script"
+  exit
+fi
+
+ls ~/.ssh | grep --silent pub
+if [ $? -eq 0 ]; then
+    echo "@>> SSH key is present, cool!"
+else
+    echo "@>> No SSH key is present in ~/.ssh"
+    echo "@>> Github SSH key is required for the script"
+    exit
+fi
