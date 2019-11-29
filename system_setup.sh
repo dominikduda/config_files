4170499a08e722902c1b561cf937661ad886b07c
dominikduda <gospodorz@gmail.com>
2019-11-29 21:02:23 +0100

    Reload bash after node install

--- system_setup.sh
+++ system_setup.sh
@@ -54,6 +54,7 @@ echo "@>> Installing NVM"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
 source ~/.bashrc
 nvm install node
+source ~/.bashrc
 
 echo "@>> Installing and setting up git"
 
