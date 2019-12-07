c8808c34b8f17bbdd6212bc1d7e9265c94a5ebce
dominikduda <gospodorz@gmail.com>
2019-12-07 18:16:43 +0100

    Install chrome in system setup

--- system_setup.sh
+++ system_setup.sh
@@ -147,6 +147,11 @@ make
 sudo make install
 cd ~/
 
+echo "@>> install chrome"
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
 echo "@>> configure system"
 
 # flux
