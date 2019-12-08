1341a2b52bc25f846c14953396ed49cdeed69295
dominikduda <gospodorz@gmail.com>
2019-12-08 14:39:12 +0100

    Insall docker in system_setup.sh

--- system_setup.sh
+++ system_setup.sh
@@ -163,6 +163,14 @@ echo "@>> install chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
+echo "@>> install docker"
+
+sudo apt --assume-yes install docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 echo "@>> setup numix circle icons"
 
 sudo add-apt-repository ppa:numix/ppa -y
