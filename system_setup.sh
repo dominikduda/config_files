ccd593f0ae52d39fa12b0c5a00ac5070c7a0562c
dominikduda <gospodorz@gmail.com>
2019-12-15 19:51:49 +0100

    Fix docker in system_setup, update system settings, install nvm in cat-tmuxinator

--- system_setup.sh
+++ system_setup.sh
@@ -209,6 +209,8 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo groupadd docker
+sudo usermod -aG docker $USER
 
 echo "@>> setup numix circle icons"
 
