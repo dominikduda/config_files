7e45394c985cc2df9219e6562a563b7b6c557a0c
dominikduda <gospodorz@gmail.com>
2020-02-09 13:59:21 +0100

    Add clear_thimbs_cache.sh; refactor backup_config_files.sh

--- bin/backup_config_files.sh
+++ bin/backup_config_files.sh
@@ -1,47 +1,48 @@
 #!/bin/bash
 
-# vim
-cp ~/.vimrc ~/github/config_files/.vimrc
-cp ~/.config/nvim/colors/dante_modified.vim ~/github/config_files/.config/nvim/colors/dante_modified.vim
+declare -a pathsFromHome=(
+  # VIM stuff
+  ".vimrc"
+  ".config/nvim/colors/dante_modified.vim"
+  # R
+  ".rstudio-desktop"
+  ".config/RStudio"
+  ".Rprofile"
+  # ZSH
+  ".zshrc"
+  ".oh-my-zsh/themes/thedd.zsh-theme"
+  # TMUX
+  ".tmux.conf"
+  ".tmux-powerlinerc"
+  ".tmux/plugins/tmux-powerline/themes/ddtheme.sh"
+  # bin
+  "bin/fix_vim_syntax_files.sh"
+  "bin/setup_md.sh"
+  "bin/backup_config_files.sh"
+  "bin/normalize_monitor_scales.sh"
+  "bin/generate_wallpaper.sh"
+  "bin/clear_thumbs_cache.sh"
+  # autostart
+  ".config/autostart/normalize_monitor_scales.desktop"
+  # custom app starters
+  ".local/share/applications/whatsapp.desktop"
+  # git
+  ".gitconfig"
+  ".gitignore"
+  # other
+  ".config/redshift.conf"
+  ".importjs.js.example"
+  # Jforex
+  "JForex/Templates/thedd.tmpl"
+)
 
-# RStudio
-cp -r ~/.rstudio-desktop ~/github/config_files/.rstudio-desktop
-cp -r ~/.config/RStudio ~/github/config_files/.config/RStudio
+for filePath in "${pathsFromHome[@]}"
+do
+  echo "@>> Backing up $filePath"
+  path_from=$HOME/$filePath
+  path_to=$HOME/github/config_files/$filePath
+  cp $path_from "$path_to"
+done
 
-# zsh
-cp ~/.zshrc ~/github/config_files/.zshrc
-cp ~/.oh-my-zsh/themes/thedd.zsh-theme ~/github/config_files/.oh-my-zsh/themes/thedd.zsh-theme
-
-# tmux
-cp ~/.tmux.conf ~/github/config_files/.tmux.conf
-cp ~/.tmux-powerlinerc ~/github/config_files/.tmux-powerlinerc
-cp ~/.tmux/plugins/tmux-powerline/themes/ddtheme.sh ~/github/config_files/.tmux/plugins/tmux-powerline/themes/ddtheme.sh
-
-# tmuxinator
+# TMUXINATOR special recursive case
 cp -r ~/.tmuxinator/. ~/github/config_files/.tmuxinator
-
-# bin
-cp ~/bin/fix_vim_syntax_files.sh ~/github/config_files/bin/fix_vim_syntax_files.sh
-cp ~/bin/setup_md.sh ~/github/config_files/bin/setup_md.sh
-cp ~/bin/backup_config_files.sh ~/github/config_files/bin/backup_config_files.sh
-cp ~/bin/normalize_monitor_scales.sh ~/github/config_files/bin/normalize_monitor_scales.sh
-cp ~/bin/generate_wallpaper.sh ~/github/config_files/bin/generate_wallpaper.sh
-
-# autostart
-cp ~/.config/autostart/normalize_monitor_scales.desktop ~/github/config_files/.config/autostart/normalize_monitor_scales.desktop
-
-# custom app starters
-
-cp ~/.local/share/applications/whatsapp.desktop ~/github/config_files/.local/share/applications/whatsapp.desktop
-
-# other
-cp ~/.config/redshift.conf ~/github/config_files/.config/redshift.conf
-cp ~/.gitconfig ~/github/config_files/.gitconfig
-cp ~/.gitignore ~/github/config_files/.gitignore
-cp ~/.importjs.js.example ~/github/config_files/.importjs.js.example
-
-# jforex
-cp ~/JForex/Templates/thedd.tmpl ~/github/config_files/JForex/Templates/thedd.tmpl
-
-# R
-cp ~/.Rprofile ~/github/config_files/.Rprofile
---
+++ bin/clear_thumbs_cache.sh
@@ -0,0 +1,3 @@
+rm ~/.cache/thumbnails/fail/gnome-thumbnail-factory/*
+rm ~/.cache/thumbnails/large/*
+rm ~/.cache/thumbnails/normal/*
